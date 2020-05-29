6f51ebcf85aaf17d6367087ebd03cc0b00f30495
Cindy <cindyzhang977@gmail.com>
2020-05-29 16:49:37 -0700

    for each look into for of loop

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,14 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import photosData from './photos-data.js';
+import photosData from "./photos-data.js";
 
 /**
- * Create photo component with caption 
+ * Create photo component with caption
  */
 function createPhoto(photo) {
-    const component =
-      `<div class="col-4"> \
+  const component = `<div class="col-4"> \
           <figure class=\"figure\"> \
               <img src=${photo.src} class=\"figure-img img-fluid rounded\" alt=${photo.location}> \
               <div class=\"row figure-caption-container\"> \
@@ -28,21 +27,19 @@ function createPhoto(photo) {
                   <figcaption class=\"figure-caption photo-date\">${photo.date}</figcaption> \
               </div> \
           </figure> \
-      </div>`
+      </div>`;
 
-  return component
+  return component;
 }
 
 /**
  * Map photos into gallery
  */
- function mapPhotos() {
-     photosData.forEach(
-        (photo) => {
-            const component = createPhoto(photo);
-            $('#gallery').append(component);
-        } 
-     )
- }
+function mapPhotos() {
+  for (const photo of photosData) {
+    const component = createPhoto(photo);
+    $("#gallery").append(component);
+  }
+}
 
- mapPhotos();
+mapPhotos();
