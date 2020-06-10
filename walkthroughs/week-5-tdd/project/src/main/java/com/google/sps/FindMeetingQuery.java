e456462ffbf0cf5b36b12a47374ba665c71ae2ad
Cindy <cindyzhang977@gmail.com>
2020-06-10 21:21:20 +0000

    query method in FindMeetingQuery

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,45 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<String> attendees = request.getAttendees();
+    long duration = request.getDuration();
+
+    // create collection of unavailable times 
+    HashSet<TimeRange> unavailableTimesSet = new HashSet<>();
+    for (Event e : events) {
+      Collection<String> eventAttendees = e.getAttendees();
+      if (!Collections.disjoint(eventAttendees, attendees)) {
+        unavailableTimesSet.add(e.getWhen());
+      }
+    }
+
+    List<TimeRange> unavailableTimesList = new ArrayList<>(unavailableTimesSet);
+    Collections.sort(unavailableTimesList, TimeRange.ORDER_BY_START);
+    List<TimeRange> meetingTimes = new ArrayList<>();
+
+    // search for possible meeting times 
+    int firstAvail = TimeRange.START_OF_DAY;
+    for (TimeRange t : unavailableTimesList) {
+      int start = t.start();
+      int end = t.end();
+      if (start - firstAvail >= duration) {
+        meetingTimes.add(TimeRange.fromStartEnd(firstAvail, start, false));
+      }
+      firstAvail = Math.max(end, firstAvail);
+    }
+
+    if (TimeRange.END_OF_DAY - firstAvail >= duration) {
+      meetingTimes.add(TimeRange.fromStartEnd(firstAvail, TimeRange.END_OF_DAY, true));
+    }
+
+    return meetingTimes;
   }
 }
